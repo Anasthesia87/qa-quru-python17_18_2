72e0fca81897ac649990246c0684606ad1c3ac85
Anastasiya_Lavretskaya <a.lavretskaya87@yandex.ru>
2025-02-03 00:35:57 +0300

    update

--- tests/ios/test_ios_wikipedia.py
+++ tests/ios/test_ios_wikipedia.py
@@ -7,9 +7,9 @@ import allure
 def test_ios_text_input(mobile_settings):
     if mobile_settings == 'android':
         pytest.skip('test for ios, not for android')
-    with allure.step('Input text'):
+    with allure.step('Type email'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, 'Text Button')).click()
-        # browser.element((AppiumBy.ACCESSIBILITY_ID, "Text Input")).send_keys('Pumpkin Eater\n')
-        browser.element((AppiumBy.ACCESSIBILITY_ID, "Text Input")).send_keys('Hello, Python!\n')
-    with allure.step('Find sended text'):
-        browser.element((AppiumBy.ACCESSIBILITY_ID, "Text Output")).should(have.text('Hello, Python!\n'))
+        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Text Input')).type('hello@browserstack.com' + '\n')
+
+    with allure.step('Verify added row'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Text Output')).should(have.text('hello@browserstack.com'))
